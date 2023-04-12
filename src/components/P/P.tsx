56f039192edd007dc9f4e7fbc155de7c5c852c16
Natalie <fogel666@yandex.ru>
2023-04-12 15:15:22 +0300

    add: package: classnames; components: P, Htag

---
+++ src/components/P/P.tsx
@@ -0,0 +1,17 @@
+import { PProps } from './P.props';
+import styles from './P.module.scss';
+import cn from 'classnames';
+
+export const P = ({ children, size = 'M', className, ...props }: PProps): JSX.Element => {
+  return (
+    <p
+      className={cn(styles.p, className, {
+        [styles.S]: size == 'S',
+        [styles.M]: size == 'M',
+      })}
+      {...props}
+    >
+      {children}
+    </p>
+  );
+};
