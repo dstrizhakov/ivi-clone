b7cd4dc0e9653516cd271b2e9f34e45f16cc6c01
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-04-14 17:20:33 +0300

    feat: add submenu component

--- src/components/Alerts/Alerts.tsx
+++ src/components/Alerts/Alerts.tsx
@@ -4,15 +4,9 @@ import { HiOutlineBellAlert } from 'react-icons/hi2';
 
 const Alerts: FC = () => {
   return (
-    <div className={styles.alerts}>
-      <HiOutlineBellAlert className={styles.alerts__button} />
-      <div className={styles.alerts__body}>
-        <span className={styles.alerts__border}></span>
-        <div className={styles.alerts__content}>
-          <HiOutlineBellAlert className={styles.alerts__icon} />
-          <p>Здесь появляются только важные сообщения</p>
-        </div>
-      </div>
+    <div className={styles.alerts__content}>
+      <HiOutlineBellAlert className={styles.alerts__icon} />
+      <p>Здесь появляются только важные сообщения</p>
     </div>
   );
 };
