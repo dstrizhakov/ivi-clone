37d780f86fda5134159f53aca421107ace73ac24
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-04-15 22:30:41 +0300

    feat: add user submenu details

--- src/components/Header/User/User.tsx
+++ src/components/Header/User/User.tsx
@@ -2,7 +2,12 @@ import React, { FC } from 'react';
 import styles from './User.module.scss';
 import { Button } from '../../Button/Button';
 import LinkCard from '@/components/Header/LinkCard/LinkCard';
-import { BiMoviePlay } from 'react-icons/bi';
+import { BiCertification, BiMoviePlay } from 'react-icons/bi';
+import { HiOutlineBookmark } from 'react-icons/hi2';
+import { IoDiamondOutline, IoTimerOutline } from 'react-icons/io5';
+import { TbDeviceTvOld } from 'react-icons/tb';
+import { GoCreditCard } from 'react-icons/go';
+import Link from 'next/link';
 
 const User: FC = () => {
   return (
@@ -10,22 +15,27 @@ const User: FC = () => {
       <div className={styles.content__row}>
         <div className={styles.content__actions}>
           <LinkCard icon={BiMoviePlay} title="Покупки" link="/pirchases" />
-          <LinkCard icon={BiMoviePlay} title="Смотреть позже" link="/pirchases" />
-          <LinkCard icon={BiMoviePlay} title="История просмотров" link="/pirchases" />
+          <LinkCard icon={HiOutlineBookmark} title="Смотреть позже" link="/pirchases" />
+          <LinkCard icon={IoTimerOutline} title="История просмотров" link="/pirchases" />
           <LinkCard
-            icon={BiMoviePlay}
+            icon={IoDiamondOutline}
             title="Покупки"
             link="/pirchases"
             subtitle="Подключить"
             status="red"
           />
-          <LinkCard icon={BiMoviePlay} title="Активация сертификата" link="/pirchases" />
-          <LinkCard icon={BiMoviePlay} title="Вход по коду" link="/pirchases" />
+          <LinkCard icon={BiCertification} title="Активация сертификата" link="/pirchases" />
+          <LinkCard icon={TbDeviceTvOld} title="Вход по коду" link="/pirchases" />
+          <LinkCard icon={GoCreditCard} title="Способы оплаты" link="/pirchases" />
         </div>
         <div className={styles.content__auth}>
           <Button size="L" appearance="red">
             <span className={styles.content__nowrap}>Войти или зарегистрироваться</span>
           </Button>
+          <div className={styles.content__links}>
+            <Link href={'https://www.ivi.tv/profile/settings'}>Настройки</Link>
+            <Link href={'https://ask.ivi.ru/'}>Помощь</Link>
+          </div>
         </div>
       </div>
     </div>
