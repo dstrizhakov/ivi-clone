6ad0f56be92b25ff5961bff0a3b76bf49924bf98
extroblade <mr.ovs0ford@gmail.com>
2023-04-16 13:43:19 +0400

    fix/modals-escape_hook-no_scroll

---
+++ src/components/Header/Search/SearchModal/SearchModal.tsx
@@ -0,0 +1,34 @@
+import React, { FC, useState } from 'react';
+import styles from './SearchModal.module.scss';
+import { CgClose } from 'react-icons/cg';
+import { IoSearchOutline } from 'react-icons/io5';
+import { SearchModalProps } from './SearchModal.props';
+import FullScreenModal from '@/components/Modals/FullScreenModal/FullScreenModal';
+
+const SearchModal: FC<SearchModalProps> = ({ isOpen, closeSearch }) => {
+  const [query, setQuery] = useState<string>('');
+
+  const clearQuery = (): void => {
+    setQuery('');
+  };
+
+  return (
+    <>
+      <FullScreenModal isOpen={isOpen} closeSearch={closeSearch}>
+        <div className={styles.body}>
+          <h3>Поиск</h3>
+          <div className={styles.input}>
+            <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+            {!!query ? (
+              <CgClose className={styles.input__icon} onClick={clearQuery} />
+            ) : (
+              <IoSearchOutline className={styles.input__icon} />
+            )}
+          </div>
+        </div>
+      </FullScreenModal>
+    </>
+  );
+};
+
+export default SearchModal;
