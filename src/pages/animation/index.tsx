1da72ba308ad49a2889cb620f58def30fc8c35d4
dstrizhakov <dstrizhakov@gmail.com>
2023-04-18 16:24:16 +0300

    feat: add Breadcrumbs.tsx

--- src/pages/animation/index.tsx
+++ src/pages/animation/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Htag } from "@/components/Htag/Htag";
 import Description from "@/components/Description/Description";
+import BreadCrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 
 const Index = () => {
+
+  const breadcrumbs = [
+    { name: 'Мой Иви', path: '/'},
+    { name: 'Мультфильмы', path: '/animation'},
+  ]
   return (
     <div>
+      <BreadCrumbs breadcrumbs={breadcrumbs}/>
       <Htag tag={'h2'}>Мультфильмы смотреть онлайн</Htag>
       <Description/>
     </div>
--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Htag } from "@/components/Htag/Htag";
 import Description from "@/components/Description/Description";
+import BreadCrumbs from "@/components/Breadcrumbs/Breadcrumbs";
 
 const Index = () => {
+
+  const breadcrumbs = [
+    { name: 'Мой Иви', path: '/'},
+    { name: 'Фильмы', path: '/movies'},
+  ]
   return (
     <div>
+      <BreadCrumbs breadcrumbs={breadcrumbs}/>
       <Htag tag={'h2'}>Фильмы смотреть онлайн</Htag>
       <Description/>
     </div>
