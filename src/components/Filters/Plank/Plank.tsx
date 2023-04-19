da6c264422f1028245c3533ca7ef37e953b6f7e4
extroblade <mr.ovs0ford@gmail.com>
2023-04-19 11:13:42 +0400

    fix: dd arrow

--- src/components/Filters/Plank/Plank.tsx
+++ src/components/Filters/Plank/Plank.tsx
@@ -1,22 +1,20 @@
 import React, { FC, useState } from 'react';
 import styles from './Plank.module.scss';
-import { BsChevronCompactDown } from 'react-icons/bs';
-import { movieCategories } from '@/mock/movieCategories';
+import { BsChevronCompactDown, BsChevronCompactUp } from 'react-icons/bs';
 
 const Plank: FC = ({ plank }) => {
   const [dropDownOpen, setDropDownOpen] = useState(false);
-  const dropdowns = movieCategories.genres;
   return (
     <>
       <button
         className={`${styles.plank} ${dropDownOpen && styles.active}`}
-        disabled={dropdowns.length <= 0}
+        disabled={plank.id == 3} //mock
         onClick={() => setDropDownOpen((dd) => !dd)}
       >
         <div className={styles.title}>{plank.title}</div>
 
         <div className={styles.icon}>
-          <BsChevronCompactDown />
+          {dropDownOpen ? <BsChevronCompactUp /> : <BsChevronCompactDown />}
         </div>
       </button>
     </>
