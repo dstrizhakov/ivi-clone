2a0751f1101af6a306e96ce596ffd025bdfb16fb
extroblade <mr.ovs0ford@gmail.com>
2023-04-20 11:15:36 +0400

    fix: carousel

---
+++ src/mock/carouselMocks.ts
@@ -0,0 +1,23 @@
+const long = 'long long long long long long',
+  short = 'short';
+const src1 =
+  'https://thumbs.dfs.ivi.ru/storage31/contents/1/a/930dc6b17e3a89b29615929bd86041.jpg/234x360/?q=85';
+const src2 =
+  'https://thumbs.dfs.ivi.ru/storage28/contents/0/8/3d2850061083ba6946ad877bc9102d.jpg/234x360/?q=85';
+export const carouselMock = [
+  { id: 1, name: long, img: src1 },
+  { id: 2, name: long, img: src2 },
+  { id: 3, name: short, img: src1 },
+  { id: 4, name: short, img: src2 },
+  { id: 5, name: long, img: src1 },
+  { id: 6, name: long, img: src2 },
+  { id: 7, name: short, img: src1 },
+  { id: 8, name: short, img: src2 },
+  { id: 9, name: long, img: src1 },
+  { id: 10, name: long, img: src2 },
+  { id: 11, name: short, img: src1 },
+  { id: 12, name: short, img: src2 },
+  { id: 13, name: long, img: src1 },
+  { id: 14, name: long, img: src2 },
+  { id: 15, name: short, img: src1 },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,48 @@
 import Head from 'next/head';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import Carousel from '@/components/Carousel/Carousel';
 import MainPageDescription from '@/components/MainPage/MainPageDescription';
+import Card from '@/components/Card/Card';
+import RatingModal from '@/components/Card/RatingModal';
+import { carouselMock } from '@/mock/carouselMocks';
 
 const Home: FC = () => {
+  const [isRatingOpen, setIsRatingOpen] = useState<boolean>(false);
+
   return (
     <>
       <Head>
         <title>Главная</title>
       </Head>
       <MainPageDescription />
-      <Carousel title={'Зарубежные сериалы'} route={'/'} />
-      <Carousel title={'Приключения'} route={'/'} showAll />
+      <Carousel title={'Зарубежные сериалы'} route={'/'}>
+        {carouselMock.map((card) => (
+          <Card
+            card={card}
+            openRating={() => setIsRatingOpen(true)}
+            star
+            book
+            find
+            block
+            key={card.id}
+          />
+        ))}
+      </Carousel>
+      <Carousel title={'Приключения'} route={'/'} star book find block showAll>
+        {carouselMock.map((card) => (
+          <Card
+            card={card}
+            openRating={() => setIsRatingOpen(true)}
+            star
+            book
+            find
+            block
+            key={card.id}
+          />
+        ))}
+      </Carousel>
+
+      <RatingModal isOpen={isRatingOpen} closeModal={() => setIsRatingOpen(() => false)} />
     </>
   );
 };
