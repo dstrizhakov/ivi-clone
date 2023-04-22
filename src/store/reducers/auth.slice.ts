ddb5e34d3ed9dd86f46d5b5e72871538cc5c438f
dstrizhakov <dstrizhakov@gmail.com>
2023-04-22 11:15:50 +0300

    refactor auth.slice

--- src/store/reducers/auth.slice.ts
+++ src/store/reducers/auth.slice.ts
@@ -4,7 +4,7 @@ import { RootState } from '../store';
 export interface IAuth {
   user: string | null;
   token: string | null;
-  role: 'user' | 'admin';
+  role: 'user' | 'admin' | null;
   favorites: string[];
   watched: string[];
 }
@@ -21,14 +21,33 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setUser: (state, action) => {
-      const { user, accessToken } = action.payload;
+    setUser: (
+      state,
+      action: PayloadAction<{
+        user: string;
+        role: 'user' | 'admin' | null;
+        favorites: string[];
+        watched: string[];
+        accessToken: string;
+      }>
+    ) => {
+      const { user, role, favorites, watched, accessToken } = action.payload;
       state.token = accessToken;
       state.user = user;
+      state.role = role;
+      state.favorites = favorites;
+      state.watched = watched;
+    },
+    logout: (state) => {
+      state.token = null;
+      state.user = null;
+      state.role = null;
+      state.favorites = [];
+      state.watched = [];
     },
   },
 });
 
 export const selectAuth = (state: RootState) => state.authReducer;
-export const { setUser } = authSlice.actions;
+export const { setUser, logout } = authSlice.actions;
 export default authSlice.reducer;
