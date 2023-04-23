b1c74c8837f3b41416f6b5c4f66cfa6918be789c
Natalie <fogel666@yandex.ru>
2023-04-23 16:56:34 +0300

    add: persons gallery

--- src/components/WatchPage/WatchPage.tsx
+++ src/components/WatchPage/WatchPage.tsx
@@ -6,6 +6,7 @@ import styles from './WatchPage.module.scss';
 import { PersonList } from './PersonList/PersonList';
 import Carousel from '../Carousel/Carousel';
 import { WatchPageProps } from './WatchPage.props';
+import { PersonsGallery } from './PersonsGallery/PersonsGallery';
 
 const WatchPage: FC<WatchPageProps> = ({ item }) => {
   const { name, enName, descr, trailer, year, countrys, rating, genres, duration, persons } = item;
@@ -19,16 +20,18 @@ const WatchPage: FC<WatchPageProps> = ({ item }) => {
             </div>
             <div className={styles.watch__info}>
               <div className={styles.watch__title}>
-                <Htag tag="h2">{`Фильм ${name} смотреть онлайн`}</Htag>
+                <Htag tag="h2">{`Фильм ${name} смотреть онлайн`}</Htag>{' '}
+                <div className={styles.watch__params}>
+                  <P>
+                    {year} {duration}
+                  </P>
+                  <P>
+                    {countrys} {genres}
+                  </P>
+                </div>
               </div>
-              <div className={styles.watch__params}>
-                <P>
-                  {year} {duration}
-                </P>
-                <P>
-                  {countrys} {genres}
-                </P>
-              </div>
+            </div>
+            <div>
               <div className={styles.watch__rating}>
                 <PersonList list={persons} rating={rating} />
               </div>
@@ -40,6 +43,7 @@ const WatchPage: FC<WatchPageProps> = ({ item }) => {
           </div>
         </div>
         <Carousel title={`С фильмом «${name}» смотрят`} route={'/'} />
+        <PersonsGallery list={persons} />
       </section>
     </>
   );
