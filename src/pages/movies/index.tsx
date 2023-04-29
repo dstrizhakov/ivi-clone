dd1c30ee0043b8fc291b02298bbd2b5facb9dc8e
extroblade <mr.ovs0ford@gmail.com>
2023-04-29 16:21:45 +0400

    add: movies

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -4,9 +4,12 @@ import Head from 'next/head';
 import MoviesPageDescription from '@/components/MoviesPage/MoviesPageDescription';
 import Filters from '../../components/Filters/Filters';
 import { useTranslation } from 'react-i18next';
+import Grid from '@/components/Grid/Grid';
+import { useSelector } from 'react-redux';
 
 const Movies = () => {
   const { t } = useTranslation();
+  const { movies } = useSelector((state) => state.movieReducer);
   const breadcrumbs = [
     { name: t('sections.my-ivi'), path: '/' },
     { name: t('sections.movies'), path: '/movies' },
@@ -19,6 +22,7 @@ const Movies = () => {
       <BreadCrumbs breadcrumbs={breadcrumbs} />
       <MoviesPageDescription />
       <Filters />
+      <Grid array={movies} />
     </>
   );
 };
