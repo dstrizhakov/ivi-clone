422152871e424b0b9b945e9269a118af988e0958
extroblade <mr.ovs0ford@gmail.com>
2023-05-06 09:43:19 +0400

    fix: remote patterns

--- next.config.js
+++ next.config.js
@@ -3,13 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com', 'sun6-22.userapi.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'thumbs.dfs.ivi.ru',
-      },
-    ],
+    domains: ['lh3.googleusercontent.com', 'sun6-22.userapi.com', 'thumbs.dfs.ivi.ru'],
   },
 };
 
