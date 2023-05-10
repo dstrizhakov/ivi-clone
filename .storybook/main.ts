18ad3e70611721f445002e558d91f0dd93632cf6
extroblade <mr.ovs0ford@gmail.com>
2023-05-10 12:32:29 +0400

    fix: storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,15 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
-const previewAnnotations: { stories: string[]; framework: { name: string; options: {} }; docs: { autodocs: string }; addons: string[] } = {
+const previewAnnotations: StorybookConfig  = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     'storybook-react-i18next',
-    'storybook-addon-next-router'
+    'storybook-addon-next-router',
+    // 'addon-redux',
+    '@tomfreudenberg/next-auth-mock/storybook'
   ],
   framework: {
     name: '@storybook/nextjs',
@@ -17,4 +19,5 @@ const previewAnnotations: { stories: string[]; framework: { name: string; option
     autodocs: 'tag',
   },
 };
+
 export default previewAnnotations;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,12 @@
-import type { Preview } from '@storybook/react';
 import i18n from './i18next.ts';
 import '@/styles/global.scss';
 import './stories.scss';
-import { RouterContext } from "next/dist/shared/lib/router-context";
+import { AppRouterContext } from "next/dist/shared/lib/app-router-context";
+import { Provider } from "react-redux";
+import { store } from "../src/store/store";
+import React from 'react';
 
-const preview: Preview = {
+const preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
@@ -16,15 +18,23 @@ const preview: Preview = {
   },
 };
 
+export const decorators = [
+  (Story) => (
+    <Provider store={store}>
+      <Story />
+    </Provider>
+  ),
+]
+
 export const parameters = {
   i18n,
-  locale: 'en',
+  locale: 'ru',
   locales: {
     ru: 'Russian',
     en: 'English',
   },
   nextRouter: {
-    Provider: RouterContext.Provider,
+    Provider: AppRouterContext.Provider,
   },
 };
 
