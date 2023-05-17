7c65692ea26c0257edc76d08c153dd5b2c69740b
extroblade <mr.ovs0ford@gmail.com>
2023-05-17 12:09:17 +0400

    fix: api

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,7 +13,7 @@ export default NextAuth({
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        username: { label: 'Username', type: 'text', placeholder: ' ' },
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials, req) {
--- src/services/movie.api.ts
+++ src/services/movie.api.ts
@@ -1,11 +1,17 @@
 import { IMovie } from '@/types/types';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export type QueryParams = { limit?: number; genre?: string; rating?: number; year?: string };
+export type QueryParams = {
+  limit?: number;
+  genre?: string;
+  rating?: number;
+  year?: string;
+};
 
 export const movieApi = createApi({
   reducerPath: 'movieApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:7000' }),
+  tagTypes: ['Movie'],
   endpoints: (build) => ({
     fetchAllMovies: build.query<IMovie[], QueryParams>({
       query: ({ limit, genre, rating, year }) => ({
@@ -17,11 +23,36 @@ export const movieApi = createApi({
           _year: year,
         },
       }),
+      providesTags: result => ['Movie'],
     }),
     fetchOneMovie: build.query<IMovie, number>({
       query: (id) => ({
         url: `/movies/${id}`,
       }),
+      providesTags: result => ['Movie'],
+    }),
+    addMovie: build.mutation<IMovie, IMovie>({
+      query: (movie) => ({
+        url: '/movies',
+        method: 'POST',
+        body: movie
+      }),
+      invalidatesTags: ['Movie'],
+    }),
+    updateMovie: build.mutation<IMovie, IMovie>({
+      query: (movie) => ({
+        url: `/movies/${movie.id}`,
+        method: 'PUT',
+        body: movie
+      }),
+      invalidatesTags: ['Movie'],
+    }),
+    deleteMovie: build.mutation<IMovie, IMovie>({
+      query: (movie) => ({
+        url: `/movies/${movie.id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Movie'],
     }),
   }),
 });
