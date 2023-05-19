1d8386c860778e3b1000062b8731cec429044579
extroblade <mr.ovs0ford@gmail.com>
2023-05-19 10:42:34 +0400

    fix: rtkq

--- src/services/person.api.ts
+++ src/services/person.api.ts
@@ -18,7 +18,7 @@ export const personApi = createApi({
         url: `/persons`,
         params: {
           _name: nameToLink(name),
-        }
+        },
       }),
       providesTags: (result) => ['Person'],
     }),
